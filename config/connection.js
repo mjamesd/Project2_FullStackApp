db5c2c4d43a706dc331b93349d28ec17caab55e6
Mbaku Starcrush <j.c.nolan9@gmail.com>
2021-12-21 22:11:42 -0800

    updating new stuff

--- config/connection.js
+++ config/connection.js
@@ -4,8 +4,9 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize('mysql://gs7m8aygxd3kstpx:ykj6ctzh8zsu7vkf@l6glqt8gsx37y4hs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/j2eo83hmfozcrwvh');
 } else {
+  console.log("Local______________________________________________________")
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
