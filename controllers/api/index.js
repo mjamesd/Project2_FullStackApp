f8064b75e72708b4e3985b3f44961534a2d67939
Mbaku Starcrush <j.c.nolan9@gmail.com>
2022-01-04 21:00:34 -0800

    adding playlist routes and seeding database with playlists

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const playlistRoutes = require('./playlistRoutes');
 
 // Prefix of these routes is '/api'
 
 router.use('/users', userRoutes);
+router.use('/playlists', playlistRoutes);
+
 
 module.exports = router;
